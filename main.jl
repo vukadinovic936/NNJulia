220996d4ec36fcf5ed3cfcd39a87e41f3bc9a5e5
vukadinovic936 <milosvuk99@gmail.com>
2021-04-26 23:06:30 -0600

    added sgd

--- main.jl
+++ main.jl
@@ -8,32 +8,65 @@ end
 
 Network(sizes) = Network(length(sizes),
                          sizes,
-                         rand.(sizes,1),
+                         rand.(view(sizes,2:length(sizes)),1),
                          [ rand( sizes[i],sizes[i+1]) for i in 1:length(sizes)-1] )
 
 function sigmoid(z)
-    return 1.0/(1.0_+ exp(-z))
+    return 1.0/(1.0 + exp(-z))
 end
 
 function feedforward(network, input)
+    # TODO: throw out the for loop and run on GPU
+    input = transpose(input)
     for i in 1:length(network.weights)
         w = network.weights[i]
         b = network.biases[i]
-        input =  w*input +b
+        input =  input * w + transpose(b)
     end
     return input
 end
 
-#function SGD(network, training_data, epohcs, mini_batch_size, eta, test_data=None)
-    #if test_data
-        #n_test = length(test_data)
-        #n = len(training_data)
-    #end
-    #for j in range(epohcs)
-        ## random shuffle training data
-        #mini_batches = [view(training_data[])]
-    #end
-#end
+function SGD(network,
+             training_data,
+             epochs,
+             mini_batch_size,
+             eta,
+             test_data=nothing)
 
-n = Network([2,2,2])
-feedforward(n,[1,1])
+    if test_data != nothing
+        n_test = length(test_data)
+        n = length(training_data)
+    end
+    for j in 1:epochs
+        # TODO: Shuffle training data
+        mini_batches = [ train_x[:,:,k:k+mini_batch_size-1] for k in 1:mini_batch_size:size(train_x)[3] ]
+        for mini_batch in mini_batches
+            # TODO create update_mini_batch
+            update_mini_batch(mini_batch, eta)
+        end
+
+        if test_data != nothing
+            # TODO add evaluate test_data
+            @printf "Epoch %d: %d / %d" j test_data n_test
+        else
+            @printf "Epoch %d: complete" j 
+        end
+    end
+end
+
+############### MAIN #################
+using MLDatasets
+using ImageView
+using Printf
+
+train_x, train_y = MNIST.traindata()
+
+print(size(train_x)) # (28,28, 60000)
+sample = train_x[:,:,2]
+sample = reshape(sample,(28*28))
+print(size(sample)) # (28,28)
+
+n = Network([784,2,2])
+feedforward(n, sample)
+mini_batch_size = 4
+#### NEXT TODO: MAKE INPUT (X,Y) where X is an image and Y a label
