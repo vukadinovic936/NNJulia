1b70a288b5fb6d96d3a03f211a0ae99e508d56a8
vukadinovic936 <milosvuk99@gmail.com>
2021-06-23 07:58:54 -0700

    changes

--- main.jl
+++ main.jl
@@ -34,13 +34,13 @@ function feedforward(network, input, keep_grad=false)
         output = output * w
 
         if keep_grad
-            append!(zs,output)
+            append!(zs,[output])
         end
 
         output = sigmoid.(output)
 
         if keep_grad
-            append!(activations, output)
+            append!(activations, [output])
         end
     end
     if keep_grad
@@ -62,7 +62,6 @@ function SGD(network::Network,
              η,
              test_data=nothing,
              test_labels=nothing)
-
     if test_data != nothing
         n_test = length(test_data)
         n = length(train_data)
@@ -87,37 +86,47 @@ function SGD(network::Network,
 end
 function update_mini_batch(network::Network, data, labels, η)
     ∇w = copy(network.weights)
-    ∇w .= 0
-    for (img,label) in zip(data,labels)
+#    ∇w .= 0.0
+    for it in 1:(size(data)[3])
+        img = @view data[:,:,it]
+        label = labels[it+1]
+        print(label)
         ∇w += backprop(network,img,label)
+        break
     end
-    print(∇W)
-    print(size(∇W))
+    print(∇w)
+    print(size(∇w))
     network.weights = network.weights - η * ∇w
     return network
 end
 
 function backprop(network::Network, x, y)
     ∇w = copy(network.weights)
-    ∇w .= 0
+#    ∇w .= 0.0
     zs, activations = feedforward(network,x,true)
-    δ = cost((@view activations[length(activations)-1]),y) * sigmoid_prime(@view zs[length(zs)-1])
-    ∇w[length(∇w)-1] = δ * transpose(@view activations[length(activations)-2])
-    for l in 2:(network.num_layers-1)
-        z = @view zs[length(zs)-l]
-        sp = sigmoid_prime(z)
-        δ = transpose(@view network.weights[length(zs)-l+1])*δ*sp
-        ∇w[length(∇w)-l] = delta * transpose(activations[length(activations)-l-1])
-    end
-    return ∇w
+    print("SIZE TIME\n")
+    print(zs[length(zs)-1])
+    print(sigmoid_prime.(zs[length(zs)-1]))
+    print(activations[length(activations)-1])
+    print(size(y))
+    print(cost((activations[length(activations)-1]),y))
+    #δ = cost((@view activations[length(activations)-1]),y) * 
+#    ∇w[length(∇w)-1] = δ * transpose(@view activations[length(activations)-2])
+#    for l in 2:(network.num_layers-1)
+#        z = @view zs[length(zs)-l]
+#        sp = sigmoid_prime(z)
+#        δ = transpose(@view network.weights[length(zs)-l+1])*δ*sp
+#        ∇w[length(∇w)-l] = delta * transpose(activations[length(activations)-l-1])
+#    end
+#    return ∇w
 end
 ############### MAIN #################
 Random.seed!(123);
 train_x, train_y = MNIST.traindata()
 sample = train_x[:,:,1]
 n = Network([784,2,2])
-feedforward(n, sample)
+feedforward(n, sample,true)
 print(size(n.weights[1]))
-reshape(n.weights,(2,)
-SGD(n,train_x,train_y,1,8,0.01)
+reshape(n.weights,(2,))
+SGD(n,train_x,train_y,1,8,0.01,)
 #### NEXT TODO: MAKE INPUT (X,Y) where X is an image and Y a labeligof
