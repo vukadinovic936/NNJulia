aa2b2905a18cb09ae201ec8edd46891da45c893f
vukadinovic936 <milosvuk99@gmail.com>
2021-06-25 07:52:01 -0700

    trained the network

--- main.jl
+++ main.jl
@@ -1,10 +1,10 @@
-
+using Random
 using MLDatasets
 using ImageView
 using Printf
 using Random
 
-struct Network
+mutable struct Network
     num_layers::Integer
     sizes::Array{Integer}
     weights::Array{Array{Float64}}
@@ -17,7 +17,15 @@ Network(sizes) = Network(length(sizes),
 function sigmoid(z)
     return 1.0/(1.0 + exp(-z))
 end
-
+function eval_single(sample)
+    mxval, mxindex =findmax(feedforward(n, sample))
+    print("Digits predicted is\n")
+    print(mxindex[2]-1)
+    print("\n")
+    print("I'm this certain that it's correct\n")
+    print(mxval)
+    print("\n")
+end
 function sigmoid_prime(z)
     return sigmoid(z)*(1-sigmoid(z))
 end
@@ -27,6 +35,7 @@ function feedforward(network, input, keep_grad=false)
     output = transpose( reshape(input, (size(input)[1]*size(input)[2]) ))
     zs = []
     activations = []
+    append!(activations,[output])
     for i in 1:length(network.weights)
         # adding 1 for biases
         output=hcat(output,1)
@@ -34,7 +43,7 @@ function feedforward(network, input, keep_grad=false)
         output = output * w
 
         if keep_grad
-            append!(zs,[output])
+            append!(zs, [output])
         end
 
         output = sigmoid.(output)
@@ -51,7 +60,7 @@ function feedforward(network, input, keep_grad=false)
 end
 
 function cost(output,labels)
-    return (output-labels)
+    return output-labels
 end
     
 function SGD(network::Network,
@@ -70,32 +79,33 @@ function SGD(network::Network,
         # TODO: Shuffle training data
         mini_batches = []
         mini_batches_labels = []
+        
         for k in 1:mini_batch_size:size(train_data)[3]
             push!(mini_batches, @view train_data[:,:,k:k+mini_batch_size-1])
             push!(mini_batches_labels, @view train_labels[k:k+mini_batch_size-1])
         end
-
-        for (mini_batch, labels) in (mini_batches,mini_batches_labels)
-            # TODO create update_mini_batch
-            res = update_mini_batch(network,mini_batch,labels,0.01)
-            break
-            #network = update_mini_batch(network, mini_batch, mini_batch_labels, η)
+        for it in 1:length(mini_batches_labels)
+            mini_batch = mini_batches[it]
+            labels = mini_batches_labels[it]
+            network = update_mini_batch(network,mini_batch,labels,0.01)
         end
 
-    end
+        print( "###############")
+        print("EPOCH DONE")
+        print(j)
+        print( "###############")
+end
 end
 function update_mini_batch(network::Network, data, labels, η)
     ∇w = copy(network.weights)
 #    ∇w .= 0.0
     for it in 1:(size(data)[3])
         img = @view data[:,:,it]
-        label = labels[it+1]
-        print(label)
-        ∇w += backprop(network,img,label)
-        break
+        # make one hot encoded
+        label = zeros(10)
+        label[labels[it]+1]=1
+        ∇w = backprop(network,img,label)
     end
-    print(∇w)
-    print(size(∇w))
     network.weights = network.weights - η * ∇w
     return network
 end
@@ -104,29 +114,29 @@ function backprop(network::Network, x, y)
     ∇w = copy(network.weights)
 #    ∇w .= 0.0
     zs, activations = feedforward(network,x,true)
-    print("SIZE TIME\n")
-    print(zs[length(zs)-1])
-    print(sigmoid_prime.(zs[length(zs)-1]))
-    print(activations[length(activations)-1])
-    print(size(y))
-    print(cost((activations[length(activations)-1]),y))
-    #δ = cost((@view activations[length(activations)-1]),y) * 
-#    ∇w[length(∇w)-1] = δ * transpose(@view activations[length(activations)-2])
-#    for l in 2:(network.num_layers-1)
-#        z = @view zs[length(zs)-l]
-#        sp = sigmoid_prime(z)
-#        δ = transpose(@view network.weights[length(zs)-l+1])*δ*sp
-#        ∇w[length(∇w)-l] = delta * transpose(activations[length(activations)-l-1])
-#    end
-#    return ∇w
+    δ = cost(reshape(activations[length(activations)],10),y)
+    ∇w[length(∇w)] = transpose(hcat(δ * activations[length(activations)-1],δ))
+
+    for l in 1:(network.num_layers-2)
+        z =  zs[length(zs)-l]
+        sp = sigmoid_prime.(z)
+        δ = (network.weights[length(zs)-l+1] * δ) #.* sp
+        δ = @view δ[1:length(δ)-1]
+        ∇w[length(∇w)-l] = transpose(hcat(δ * activations[length(activations)-l-1],δ))
+    end
+    return ∇w
 end
-############### MAIN #################
-Random.seed!(123);
+############### MAIN ################# Random.seed!(123);
 train_x, train_y = MNIST.traindata()
-sample = train_x[:,:,1]
-n = Network([784,2,2])
-feedforward(n, sample,true)
-print(size(n.weights[1]))
+test_x,test_y = MNIST.testdata()
+sample = test_x[:,:,10]
+correct_label= test_y[10]
+imshow(sample)
+print(correct_label)
+eval_single(sample)
+#n = Network([784,30,10])
+feedforward(n, sample,false)
 reshape(n.weights,(2,))
-SGD(n,train_x,train_y,1,8,0.01,)
+SGD(n,train_x,train_y,1,8,0.01)
+
 #### NEXT TODO: MAKE INPUT (X,Y) where X is an image and Y a labeligof
